0afbccdbedaaf3c37d03d9c26abce202b6cbe73b
Sergey Gultyayev <gultyayev.sergey@gmail.com>
2025-12-21 12:37:13 +0100

    Add Jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+import { createCjsPreset } from "jest-preset-angular/presets/index.js";
+
+// Uncomment and add packages that need to be transformed (e.g., ESM-only packages)
+// const packagesToTransform = [
+//   "@angular",
+//   "@ngrx",
+// ];
+// const transformIgnorePatterns = [
+//   `node_modules/(?!.pnpm|(${packagesToTransform.join("|")}))`,
+// ];
+
+export default {
+  ...createCjsPreset(),
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  // transformIgnorePatterns,
+} satisfies Config;
---
+++ src/app/app.spec.ts
@@ -0,0 +1,16 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { App } from "./app";
+
+describe("App", () => {
+  let fixture: ComponentFixture<App>;
+  let component: App;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+  });
+
+  it("should create the app", () => {
+    expect(component).toBeTruthy();
+  });
+});
