9b4288aea916dbca90dbece63159b90a7d12e02e
Sergey Gultyayev <gultyayev.sergey@gmail.com>
2025-12-21 12:38:12 +0100

    Add Storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,33 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import eslintConfigAngular from "@epam/eslint-config-angular";
 
-export default [
-  {
-    ignores: [
-      "*.config.ts",
-      "setup-jest.ts",
-      ".storybook/",
-      "src/stories/",
+export default [{
+  ignores: [
+    "*.config.ts",
+    "setup-jest.ts",
+    ".storybook/",
+    "src/stories/",
+  ],
+}, ...eslintConfigAngular, {
+  files: ["**/*.ts"],
+  rules: {
+    "@angular-eslint/directive-selector": [
+      "error",
+      {
+        type: "attribute",
+        prefix: "app",
+        style: "camelCase",
+      },
+    ],
+    "@angular-eslint/component-selector": [
+      "error",
+      {
+        type: "element",
+        prefix: "app",
+        style: "kebab-case",
+      },
     ],
   },
-  ...eslintConfigAngular,
-  {
-    files: ["**/*.ts"],
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
-    },
-  },
-];
+}, ...storybook.configs["flat/recommended"]];
