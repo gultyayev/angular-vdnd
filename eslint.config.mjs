5b4f2ced83612599b8751b30d3393ff270bc3c08
Sergey Gultyayev <gultyayev.sergey@gmail.com>
2025-12-21 12:36:53 +0100

    Add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import eslintConfigAngular from "@epam/eslint-config-angular";
+
+export default [
+  {
+    ignores: [
+      "*.config.ts",
+      "setup-jest.ts",
+      ".storybook/",
+      "src/stories/",
+    ],
+  },
+  ...eslintConfigAngular,
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+];
